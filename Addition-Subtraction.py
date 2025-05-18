1d67d825fe31a8dd7479b1365a1c258d02fbd2e5
AlbrumAI <brumfield.alicia@yahoo.com>
2025-05-18 16:49:35 -0500

    Added math operations program

---
+++ Addition-Subtraction.py
@@ -0,0 +1,15 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Part 1: Addition and Subtraction
+>>> num1 = float(input("Enter first number: "))
+Enter first number: 10
+>>> num2 = float(input("Enter second number: "))
+Enter second number: 20
+>>> 
+>>> addition = num1 + num2
+>>> subtraction = num1 - num2
+>>> 
+>>> print(f"Addition Result: {addition}")
+Addition Result: 30.0
+>>> print(f"Subtraction Result: {subtraction}")
+Subtraction Result: -10.0
